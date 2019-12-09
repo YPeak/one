1f1d41c3a106101b07e45475ebe6952252b5a44e
yu <yushuwei178@163.com>
2019-12-09 11:51:03 +0800

    1

--- src/main/java/com/yu/theone/aop/BaseAop.java
+++ src/main/java/com/yu/theone/aop/BaseAop.java
@@ -6,7 +6,6 @@ import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
